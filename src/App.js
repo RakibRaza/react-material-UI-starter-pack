065c220ff28989a58956c1658e83279f0f1bd68c
Rakib Raza <rakibraza159@gmail.com>
2021-03-17 02:34:38 +0600

    theme update

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import { CssBaseline } from "@material-ui/core";
+import { CssBaseline, ThemeProvider } from "@material-ui/core";
 import NavBar from "./components/NavBar/NavBar";
 import News from "./components/News/News";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { theme } from "./theme";
 import SingleNews from "./components/SingleNews/SingleNews";
 import Footer from "./components/Footer/Footer";
 import About from "./components/Pages/About";
 function App() {
   return (
-    <Router>
-      <NavBar />
-      <Switch>
-        <Route exact path="/">
-          <News />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/news/:params">
-          <SingleNews />
-        </Route>
-      </Switch>
-      <Footer />
-      <CssBaseline />
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <News />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/news/:params">
+            <SingleNews />
+          </Route>
+        </Switch>
+        <Footer />
+        <CssBaseline />
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,6 +2,7 @@ import {
   AppBar,
   Button,
   Container,
+  Hidden,
   IconButton,
   makeStyles,
   Menu,
@@ -13,19 +14,7 @@ import MenuIcon from "@material-ui/icons/Menu";
 import React, { useState } from "react";
 import Scroll from "../Scroll/Scroll";
 import { Link } from "react-router-dom";
-const useStyles = makeStyles((theme) => ({
-  sectionDesktope: {
-    display: "none",
-    [theme.breakpoints.up("md")]: {
-      display: "flex",
-    },
-  },
-  mobileMenu: {
-    [theme.breakpoints.up("md")]: {
-      display: "none",
-    },
-  },
-}));
+const useStyles = makeStyles((theme) => ({}));
 
 const NavBar = () => {
   const classes = useStyles();
@@ -47,7 +36,7 @@ const NavBar = () => {
             <Typography style={{ flexGrow: 1 }} variant="h5">
               BBC News
             </Typography>
-            <div className={classes.sectionDesktope}>
+            <Hidden smDown>
               <Button component={Link} to="/" color="inherit">
                 Home
               </Button>
@@ -55,25 +44,23 @@ const NavBar = () => {
                 About
               </Button>
               <Button color="inherit">Contact</Button>
-            </div>
-            <IconButton
-              className={classes.mobileMenu}
-              onClick={handleClick}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              className={classes.mobileMenu}
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={handleClose}
-            >
-              <MenuItem onClick={handleClose}>Home</MenuItem>
-              <MenuItem onClick={handleClose}>About</MenuItem>
-              <MenuItem onClick={handleClose}>Contact</MenuItem>
-            </Menu>
+            </Hidden>
+            <Hidden mdUp>
+              <IconButton onClick={handleClick} color="inherit">
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Home</MenuItem>
+                <MenuItem onClick={handleClose}>About</MenuItem>
+                <MenuItem onClick={handleClose}>Contact</MenuItem>
+              </Menu>
+            </Hidden>
           </Toolbar>
+
           <Scroll showBelow={250} />
         </Container>
       </AppBar>
